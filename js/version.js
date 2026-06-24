1f72a09b239fb0710d3922dd294f37e66f0cad76
Marcio <marcio.v.souza@icloud.com>
2026-06-23 23:48:53 -0300

    chore: bump ASSET_VERSION 1200→1201 (modo claro Vyn)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  *
  * Como incrementar: 1201 → 1202 → 1203 ...
  */
-const ASSET_VERSION = '1200';
+const ASSET_VERSION = '1201';
 
 (function () {
   // Aplica ?v= em todos os link[rel=stylesheet] e script[src] que apontam
